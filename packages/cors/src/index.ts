7818558ac8e68a539c4dae9cc28290fe7e19e58b
concefly <concefly@foxmail.com>
2021-05-17 11:25:49 +0800

    feat: cors extension

---
+++ packages/cors/src/index.ts
@@ -0,0 +1,12 @@
+import { BaseExtension } from 'ah-server';
+import * as cors from 'koa-cors';
+
+export class CorsExtension extends BaseExtension {
+  constructor(private opts?: cors.Options) {
+    super();
+  }
+
+  getMiddleware() {
+    return [cors(this.opts)];
+  }
+}
